e0c7529afbe506b64ce3804dadb1059c3004fcef
Varun-Babu <itsme.varun.26@gmail.com>
2023-01-24 09:33:30 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddPatientComponent } from './add-patient/add-patient.component';
+import { SearchPatientComponent } from './search-patient/search-patient.component';
+import { ViewPatientComponent } from './view-patient/view-patient.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddPatientComponent,
+    SearchPatientComponent,
+    ViewPatientComponent,
+    NavbarComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
